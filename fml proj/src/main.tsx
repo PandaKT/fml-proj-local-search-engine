fec222153ff4cfd7dfd0fd3b46bc78a0e6e186e2
Prayas-Agrawal <prayasagrawal19@gmail.com>
2023-10-16 09:30:00 +0530

    setup routes and pages scaffold

--- fml proj/src/main.tsx
+++ fml proj/src/main.tsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import { Root } from "./pages/root";
+import ErrorPage from "./error-page";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Home } from "./pages/home/home";
+import { StorePage } from "./pages/store/StorePage";
+import { ItemPagePublic } from "./pages/item/itemPagePublic";
+import { StyledEngineProvider } from "@mui/material";
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "store/id/:storeID",
+        element: <StorePage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "store/id/:storeID/item/id/:itemID",
+        element: <ItemPagePublic />,
+        errorElement: <ErrorPage />,
+      },
+      // {
+      //   path: "profile/",
+      //   element: (
+      //     <Protected>
+      //       <Profile />
+      //     </Protected>
+      //   ),
+      //   errorElement: <ErrorPage />,
+      // },
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <StyledEngineProvider injectFirst>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StyledEngineProvider>
+  </StrictMode>
+);
---
+++ fml proj/src/pages/item/itemPagePublic.tsx
@@ -0,0 +1,3 @@
+export function ItemPagePublic(props: any) {
+  return <></>;
+}
