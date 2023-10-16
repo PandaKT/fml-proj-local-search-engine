2ad1cb1467ca3fef203c7628306ad6521f4259c5
Prayas-Agrawal <prayasagrawal19@gmail.com>
2023-10-16 12:30:00 +0530

    header bar and sidebar for homepage

--- fml proj/src/pages/root.tsx
+++ fml proj/src/pages/root.tsx
@@ -1,3 +1,145 @@
-export function Root(props: any) {
-  return <></>;
+import { Outlet, useNavigate } from "react-router-dom";
+
+import * as React from "react";
+import { Box, ThemeProvider, createTheme } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import { Home, Add } from "@mui/icons-material";
+import { MyAppBar } from "../components/appbar/Appbar";
+import { ScrollRestoration } from "react-router-dom";
+import { useState } from "react";
+const drawerWidth = 240;
+
+interface Props {
+  window?: () => Window;
 }
+
+export function Root(props: Props) {
+  const { window } = props;
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const navigate = useNavigate();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const rootElement = document.getElementById("root");
+  const theme = createTheme({
+    components: {
+      MuiPopover: {
+        defaultProps: {
+          container: rootElement,
+        },
+      },
+      MuiPopper: {
+        defaultProps: {
+          container: rootElement,
+        },
+      },
+      MuiDialog: {
+        defaultProps: {
+          container: rootElement,
+        },
+      },
+      MuiModal: {
+        defaultProps: {
+          container: rootElement,
+        },
+      },
+    },
+  });
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        <ListItem key={"home"} disablePadding>
+          <ListItemButton onClick={() => navigate("/")}>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary={"Home"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={"addStore"} disablePadding>
+          <ListItemButton onClick={() => setDialogOpen(true)}>
+            <ListItemIcon>
+              <Add />
+            </ListItemIcon>
+            <ListItemText primary={"Create My Store"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        {/* <MyAppBar handleDrawerToggle={handleDrawerToggle} /> */}
+        <MyAppBar />
+
+        <ScrollRestoration />
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: "none", sm: "block" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+// loading anim
+// profile
+// store page touches
+// item page touches
+// download link
+// drawer highlight
+// images and data fix
